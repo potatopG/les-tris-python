4fbea8339a3f17adcb6dc3a8d8234c7a85dd8bd7
potatopG <152330244+potatopG@users.noreply.github.com>
2024-02-18 17:04:18 +0100

    Update trirapide.py

--- les tris/trirapide.py
+++ les tris/trirapide.py
@@ -1,17 +1,13 @@
-from random import randint
-
 def tri_rapide(l):
     if len(l)<=1:
         return l
     else:
-        pipi,caca=[],[]
+        l1,l2=[],[]
         pivot =l[-1]
         for i in range(len(l)-1):
             if l[i]<l[-1]:
-                pipi.append(l[i])
+                l1.append(l[i])
             else:
-                caca.append(l[i])
-        return tri_rapide(pipi)+[l[-1]]+tri_rapide(caca)
+                l2.append(l[i])
+        return tri_rapide(l1)+[l[-1]]+tri_rapide(l2)
 
-l=[5,7,6,9,22,4,1,333]
-print(tri_rapide(l))
