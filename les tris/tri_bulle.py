3be22b4a9dfed60333fe911fdc6e91a94bd7e5de
potatopG <152330244+potatopG@users.noreply.github.com>
2024-02-18 17:16:53 +0100

    Update tri_bulle.py

--- les tris/tri_bulle.py
+++ les tris/tri_bulle.py
@@ -1,6 +1,6 @@
 def tri_bubu(l):
     indice=0
-    while indice<(len(l)-1):
+    while indice<(len(l)-1):#on fait des echanges a partir du debut de la liste jusqu'a ce que la liste soit triée
         if l[indice+1]>l[indice]:
             l[indice],l[indice+1] = l[indice+1],l[indice]
             indice=0
@@ -15,7 +15,7 @@ def tri_bulle(l):
     verif=True                                                   
     while verif:
         verif=False
-        for k in range(1,len(l)):
+        for k in range(1,len(l)):#on tri jusqu'a ce que verif soit faux
             if l[k]<l[k-1]:
                 l[k], l[k - 1] = l[k - 1], l[k]
                 verif =True                                                 
